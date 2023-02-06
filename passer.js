d8628082126e72a52a38211dd207b5e48cc943b7
bangoumouna12 <124571764+bangoumouna12@users.noreply.github.com>
2023-02-06 17:43:16 +0000

    Add files via upload

---
+++ passer.js
@@ -0,0 +1,24 @@
+  var age = document.querySelector('#age');
+function majeur()                                    
+{ 
+ 
+   
+
+   var nombre;
+   nombre=parseInt(age.value);
+   console.log(nombre)
+    if (nombre>18)                        
+    { 
+        alert("vous etes  majeur"); 
+        
+    }    
+    else
+     { 
+        alert("vous etes mineur"); 
+        
+    } 
+    
+  }  
+ 
+
+         
